44b6cbbc9c2c5faf6a67c61553ebab7988bc8677
Vigamy <andreottiwill@gmail.com>
2024-04-28 13:41:28 -0300

    Adicionando splashScreen e carrinho, preparando o design geral.

---
+++ appEden/app/src/main/java/com/eden/Carrinho.java
@@ -0,0 +1,13 @@
+package com.eden;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Carrinho extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carrinho);
+    }
+}
