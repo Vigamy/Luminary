73551fc69d7fc8839da3a157348a40684c2b0a35
leonardolinsz <leonardolinsz>
2024-08-16 18:01:31 -0300

    Corrigindo erro de import

--- Crud/EdenCrud/src/Conexoes.java
+++ Crud/EdenCrud/src/Conexoes.java
@@ -1,4 +1,8 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.DriverManager;
 
 public class Conexoes{
     private Connection conexao;
